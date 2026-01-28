959c65d44c3029a344810fb8edcb439a8e80ed34
user <user@localhost.localdomain>
2026-01-28 19:02:05 +0100

    Update: remove old files and improve structure

--- pytm/functions.py
+++ pytm/functions.py
@@ -14,36 +14,36 @@ def safe_input_menu(prompt):
         value = input(prompt).strip()
         if value:
             return value
-        print(print_spec.COLOR["error"], print_spec.ICON["error"],": ",print_spec.errors["002"])
+        print(print_spec.COLOR["error"] + print_spec.ICON["error"] + ": " + print_spec.errors["002"])
 
 def wim():  # wim = where i am?
     cwd = os.getcwd()
-    print(print_spec.COLOR["info"], print_spec.small_talks_en["1"], " ", cwd)
+    print(print_spec.COLOR["info"] + print_spec.small_talks_en["1"] + " " + cwd)
     return cwd
 
 def change(path_to_go):
     if os.path.isdir(path_to_go):
-        print(print_spec.COLOR["ok"], print_spec.small_talks_en["2"], path_to_go)
+        print(print_spec.COLOR["ok"] + print_spec.small_talks_en["2"] + path_to_go)
         print("=" * width)
         os.chdir(path_to_go)
         wim()
     else:
-        print(print_spec.COLOR["error"], print_spec.ICON["error"], print_spec.errors["003"])
+        print(print_spec.COLOR["error"] + print_spec.ICON["error"], print_spec.errors["003"])
 
 def change_short(path_for_short):
     current_path = wim()
     full_path = os.path.join(current_path, path_for_short) #  обьединение путей через библиотеку
 
     if os.path.isdir(full_path):
-        print(print_spec.COLOR["info"], print_spec.small_talks_en["1"], path_for_short)
+        print(print_spec.COLOR["info"] + print_spec.small_talks_en["1"] + path_for_short)
         print("=" * width)
         os.chdir(full_path)
         wim()
     else:
-        print(print_spec.COLOR["error"], print_spec.ICON["error"], " ", print_spec.errors["001"])
+        print(print_spec.COLOR["error"] + print_spec.ICON["error"] + " " + print_spec.errors["001"])
 
 def go_up():
-    print(print_spec.COLOR["ok"], print_spec.small_talks_en["3"])
+    print(print_spec.COLOR["ok"] + print_spec.small_talks_en["3"])
     print("=" * width)
     os.chdir('../')
     wim()
@@ -67,14 +67,14 @@ def go_to_folder():
             options[option]()
             break
         else:
-            print(print_spec.COLOR["error"], print_spec.ICON["error"], print_spec.errors["004"])
+            print(print_spec.COLOR["error"] + print_spec.ICON["error"] + print_spec.errors["004"])
 
 
 
 def list_files():
     files = os.listdir()
     if not files:
-        print(print_spec.COLOR["warn"], print_spec.small_talks_en["7"])
+        print(print_spec.COLOR["warn"] + print_spec.small_talks_en["7"])
     else:
         for file in files:
             print(f" {print_spec.ICON['folder']} {file}" if os.path.isdir(file) else f" {print_spec.ICON['file']} {file}")
@@ -100,7 +100,7 @@ def remove_folder():
 
 def openfile(file_name):
     if not os.path.isfile(file_name):
-        print(print_spec.COLOR["error"], print_spec.ICON["error"], print_spec.errors["005"])
+        print(print_spec.COLOR["error"] + print_spec.ICON["error"] + print_spec.errors["005"])
         return
 
     try:
